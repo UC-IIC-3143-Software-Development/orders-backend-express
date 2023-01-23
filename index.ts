7359c11c351e7354671cbf41204425b5e7a01ec2
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2023-01-22 21:17:26 -0300

    feat(user): add skeleton for user CRUD using prisma and docker-compose

--- index.ts
+++ index.ts
@@ -1,13 +1,40 @@
-import express, { Express, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import prisma from "./prisma";
 
 dotenv.config();
 
-const app: Express = express();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get("/health", (req: Request, res: Response) => {
+  res.send("Ok");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.post("/user", async (req, res) => {
+  const { firstName, lastName } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      firstName,
+      lastName,
+    },
+  });
+  res.json(user);
+});
+
+app.put("/users/:id", async (req, res) => {
+  console.log(req.body);
+  console.log(req.params);
+  res.json("bye");
 });
 
 app.listen(port, () => {
